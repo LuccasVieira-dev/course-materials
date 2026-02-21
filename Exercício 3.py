9ae02a3d1cd6e89b7ffb26b0e3aecfa6a8c565d4
LuccasVieira-dev <luccasvieira0147@gmail.com>
2026-02-21 04:47:46 -0400

    Refactor input handling and expression evaluation

--- Exercício 3.py
+++ Exercício 3.py
@@ -1,6 +1,9 @@
 #Exercício 3: Faça um programa que peça dois números e imprima a soma.
 
-expressao = input("Digite o cálculo: ")
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+expressao = f"{n1} + {n2}"
 
 resultado = eval(expressao, {"__builtins__": None}, {})
 
