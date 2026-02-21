5db1b02fe5edb514c305697a6bbbcaa45e37238e
LuccasVieira-dev <luccasvieira0147@gmail.com>
2026-02-21 05:04:59 -0400

    Change input to float and improve output format
    Updated input to accept float values and simplified output.

--- Exercício 3.py
+++ Exercício 3.py
@@ -1,10 +1,7 @@
 #Exercício 3: Faça um programa que peça dois números e imprima a soma.
 
-n1 = int(input("Digite o primeiro número: "))
-n2 = int(input("Digite o segundo número: "))
+n1 = float(input("digite o primeiro número: "))
+n2 = float(input("digite o segundo número: "))
+res = n1 + n2
 
-expressao = f"{n1} + {n2}"
-
-resultado = eval(expressao, {"__builtins__": None}, {})
-
-print(resultado)
+print(f"Resultado: {res:g}")
