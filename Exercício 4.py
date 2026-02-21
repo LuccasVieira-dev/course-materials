95d815ee9d4039aa6d38815ca9166222b7ca0d1e
LuccasVieira-dev <luccasvieira0147@gmail.com>
2026-02-21 05:08:56 -0400

    Refactor input and calculation of average

--- Exercício 4.py
+++ Exercício 4.py
@@ -1,12 +1,10 @@
 #Exercício 4: Faça um programa que peça dois números e imprima a soma.
 
-n1 = input("Digite a primeira nota: ")
-n2 = input("Digite a segunda nota: ")
-n3 = input("Digite a terceira nota: ")
-n4 = input("Digite a quarta nota: ")
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
 
-expressao = f"({n1} + {n2} + {n3} + {n4}) / 4"
+media = (n1 + n2 + n3 + n4) / 4
 
-media = eval(expressao, {"__builtins__": None}, {})
-
-print("A média é:", media)
+print(f"A média é: {media:g}")
