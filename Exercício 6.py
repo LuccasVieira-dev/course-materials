80a9b3f3d1576c3f3aa499cf474e10744d5e48e4
LuccasVieira-dev <luccasvieira0147@gmail.com>
2026-02-21 05:14:36 -0400

    Add program to calculate circle area from radius

---
+++ Exercício 6.py
@@ -0,0 +1,6 @@
+#Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = float(input("Raio do círculo: "))
+area = 3.14159 * raio**2
+
+print(f"Área é igual a: {area:g}")
