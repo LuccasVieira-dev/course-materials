7f1562a4f518c1e98100ab4b202a43e587ac53c0
LuccasVieira-dev <luccasvieira0147@gmail.com>
2026-02-22 00:54:23 -0400

    Add temperature conversion from Celsius to Fahrenheit

---
+++ Exercício 10.py
@@ -0,0 +1,7 @@
+#Faça um programa que peça a temperatura em graus Celsius, transforme e mostre a temperatura em graus Fahrenheit.
+
+c = float(input("Digite a temperatura em Celsius: "))
+
+f = 1.8 * c + 32
+
+print(f"A temperatura em Fahrenheit é: {f:.0f}°F")
