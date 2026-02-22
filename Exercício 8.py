42957aa24c72f2f15c242cdbb1c2914372fab0e3
LuccasVieira-dev <luccasvieira0147@gmail.com>
2026-02-22 00:26:22 -0400

    Adiciona cálculo de salário mensal
    Implementa cálculo do salário mensal com base no ganho por hora e horas trabalhadas.

---
+++ Exercício 8.py
@@ -0,0 +1,8 @@
+#Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+ganho_por_hora = float(input("Quanto você ganha por hora? "))
+horas_trabalhadas = float(input("Quantas horas você trabalhou no mês? "))
+
+salario_total = ganho_por_hora * horas_trabalhadas
+
+print(f"O seu salário este mês é: R$ {salario_total:g}")
